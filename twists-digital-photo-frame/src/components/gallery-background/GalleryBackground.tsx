756a8afc597099863b63afbbe51590dc610ace70
OliverKuhnNbg <oliver.o.kuhn@gmail.com>
2025-08-02 18:04:30 +0200

    optimize comment

--- twists-digital-photo-frame/src/components/gallery-background/GalleryBackground.tsx
+++ twists-digital-photo-frame/src/components/gallery-background/GalleryBackground.tsx
@@ -2,8 +2,8 @@ import { usePhotoStore } from '../../store/photoStore';
 import './GalleryBackgroundComponent.scss';
 
 export const Gallery = () => {
-  // Holt den Zustand bzw. die State-Werte aus dem Store und setzt
-  // Über die Werte wir ddas das aktuelle Bild Selektiert.
+  // Holt den Zustand bzw. die State-Werte aus dem Store und
+  // Über die Store-State-Werte wird das aktuelle Bild selektiert und zugewiesen
   // Die Komponente rendert jetzt nur neu, wenn sich dieser Wert ändert.
   const currentImage: string = usePhotoStore(
     (state) => state.imageUrls[state.currentIndex],
