f05692112cdc3f6e91697cf779fb47528e2a01fb
Kuhn <oliver.kuhn@adesso.de>
2025-11-04 14:15:18 +0100

    start implementing paging indicators

--- twists-digital-photo-frame/src/components/gallery-paging-indicator/PagingIndicators.tsx
+++ twists-digital-photo-frame/src/components/gallery-paging-indicator/PagingIndicators.tsx
@@ -1,10 +1,13 @@
+import { usePhotoStore } from "../../store/photoStore";
+
 export const PagingIndicators= () => {
-  
+  const imageUrls :string[] = usePhotoStore((state) => state.imageUrls);
+  const currentImageIndex :number = usePhotoStore((state) => state.currentIndex);
 
   return (
     // wrapper for the GUI-Indicator elements
     <div className="position-absolute top-0 start-0 w-100 h-100 d-flex justify-content-center mt-4">
-      Test
+      {imageUrls.length} - {currentImageIndex+1}
     </div>
   );
 };
